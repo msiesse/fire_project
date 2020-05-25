40080418b52ce8bdfca4229a668dba7d91cdc466
riesartman <martinsiesse@gmail.com>
2020-05-25 14:00:46 +0200

    first commit

---
+++ escalier/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace escalier
+{
+    class Program
+    {
+        static void PrintFloors(int floors)
+        {
+            for (int i = 0; i < floors; i++)
+            {
+                for (int j = 0; j < floors; j++)
+                {
+                    if ((i + j) >= (floors - 1))
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int floors;
+
+            if (args.Length == 0)
+            {
+                floors = 0;
+            }
+            else
+            {
+                floors = Convert.ToInt32(args[0]);
+            }
+
+            PrintFloors(floors);
+        }
+    }
+}
