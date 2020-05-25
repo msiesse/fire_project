54bfa1fe5097337752a6d8e938cb08665293cab0
riesartman <martinsiesse@gmail.com>
2020-05-25 14:35:21 +0200

    Ajout du projet factoriel

---
+++ factoriel/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace factoriel
+{
+    class Program
+    {
+        static double factoriel(double num)
+        {
+            if (num == 0)
+                return (1);
+            else if (num == 1)
+                return (1);
+            else if (num < 0)
+                return (0);
+            else
+                return num * factoriel(num - 1);
+        }
+        static void Main(string[] args)
+        {
+            double num;
+
+            if (args.Length == 0)
+                num = 0;
+            else
+                num = Convert.ToDouble(args[0]);
+
+            Console.WriteLine(factoriel(num));
+        }
+    }
+}
