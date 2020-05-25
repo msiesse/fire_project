830a56ace141f093c910888fe5e4ba9d70a0c6a8
riesartman <martinsiesse@gmail.com>
2020-05-25 14:26:58 +0200

    Ajout du projet majuscule

---
+++ majuscule/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace majuscule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Caption;
+            StringBuilder sb;
+
+            if (args.Length == 0)
+            {
+                return;
+            }
+            else
+            {
+                Caption = args[0];
+            }
+
+            sb = new StringBuilder(Caption);
+            for (int i = 0; i < Caption.Length; i++)
+            {
+                if ((i % 2) == 0)
+                {
+                    sb[i] = char.ToLower(Caption[i]);
+                }
+                else
+                {
+                    sb[i] = char.ToUpper(Caption[i]);
+                }
+            }
+            Caption = sb.ToString();
+            Console.WriteLine(Caption);
+        }
+    }
+}
