3051af348202e2ff8fda14d00924fcd68346cf85
riesartman <martinsiesse@gmail.com>
2020-05-26 13:06:39 +0200

    Ajout de big factoriel

---
+++ factoriel_big/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Numerics;
+
+namespace factoriel_big
+{
+    class Programm
+    {
+        static BigInteger factoriel(BigInteger num)
+        {
+            if (num == 0)
+                return (1);
+            else if (num == 1)
+                return (1);
+            else if (num < 0)
+                return (0);
+            else
+                return num * factoriel(num - 1);
+        }
+        static void Main(string[] args)
+        {
+            BigInteger bigNum;
+            long num;
+
+            if (args.Length == 0 || !double.TryParse(args[0], out _))
+                num = -1;
+            else
+                num = Convert.ToInt64(args[0]);
+
+            bigNum = new BigInteger(num);
+            Console.WriteLine(factoriel(bigNum));
+        }
+    }
+}
